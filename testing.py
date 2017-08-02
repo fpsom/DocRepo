83e16e4360476e1ca32551a021e807a60202f499
Fotis E. Psomopoulos <fpsom@issel.ee.auth.gr>
2017-08-02 10:57:51 +0300

    adding test files

---
+++ testing.py
@@ -0,0 +1,16 @@
+print ("Hello World!")
+
+
+#01-02.py
+
+thetext = input("Enter some text ")
+print ("This is what you entered:")
+print (thetext)
+
+
+#01-03.py
+
+# Note that \n within quote marks forces a new line to be printed
+thetext = input("Enter some text\n")
+print ("This is what you entered:")
+print (thetext)
